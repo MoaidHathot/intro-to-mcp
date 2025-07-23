97e9b37b277557683252bdb087e7c24657f92222
Moaid Hathot <Moaid.Hathot@outlook.com>
2025-07-22 23:59:41 -0700

    add client

---
+++ src/sample-mcp-client/Program.cs
@@ -0,0 +1,30 @@
+﻿using Dumpify;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Client;
+
+using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+{
+	builder
+	.AddConsole()
+	.SetMinimumLevel(LogLevel.Information);
+});
+
+var clientTransport = new StdioClientTransport(new StdioClientTransportOptions
+{
+	Name = "test-sample-client",
+	Command = "dotnet",
+	Arguments = ["run", "--project", "./sample-mcp-server"],
+});
+
+var client = await McpClientFactory.CreateAsync(clientTransport, loggerFactory: loggerFactory);
+
+(await client.ListToolsAsync()).Select(t => (name: t.Name, description: t.Description)).Dump();
+
+var result = await client.CallToolAsync(
+	"get_recipe",
+	new Dictionary<string, object?>() { 
+		{ "recipeName", "falafel" }
+	},
+	cancellationToken:CancellationToken.None);
+
+result.Content.Dump();

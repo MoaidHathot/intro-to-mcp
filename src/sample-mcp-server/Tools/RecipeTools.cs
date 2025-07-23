e27a10070217e4935f251587d5577d134664d816
Moaid Hathot <Moaid.Hathot@outlook.com>
2025-07-22 22:53:25 -0700

    adding recipe

---
+++ src/sample-mcp-server/Tools/RecipeTools.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel;
+using Dumpify;
+using ModelContextProtocol.Server;
+
+public class RecipeTools
+{
+	private const string RecipeLocation = @"C:\Users\moaid\OneDrive\Documents\Obsidian Vault\Cooking";
+
+    [McpServerTool]
+    [Description("Returns a recipe according to the recipe or food name")]
+    public object? GetRecipe([Description("The name of the recipe or the food required")] string recipeName)
+    {
+		var files = System.IO.Directory.EnumerateFiles(RecipeLocation, "*.md", System.IO.SearchOption.AllDirectories)
+			.Select(filePath => (name: filePath, content: System.IO.File.ReadAllText(filePath)))
+			.Where(tuple  => tuple.name.Contains(recipeName, StringComparison.OrdinalIgnoreCase) || tuple.content.Contains(recipeName, StringComparison.OrdinalIgnoreCase))
+			.Select(tuple => new { 
+				Name = System.IO.Path.GetFileNameWithoutExtension(tuple.name), 
+				Content = tuple.content 
+			});
+
+		var file = files.FirstOrDefault();
+		return file;
+    }
+}
